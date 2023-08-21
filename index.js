33c86b6ecf6f1411b9af47e7085ee7ccccbcf91a
festerduck <festerduck@gmail.com>
2023-08-21 23:28:44 +0500

    API is working

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log(44);
+function content(data) {
+  const currentLocation = document.getElementById("current-location");
+  const currentTemp = document.getElementById("current-temp");
+  currentLocation.innerText = data[0].name;
+  const temp = data[1].temp.toFixed(0);
+  currentTemp.innerText = temp;
+  console.log(data[0].name);
+}
+
+const apiUrl = "http://localhost:3000/fetchedData";
+
+async function fetchData() {
+  const res = await fetch(apiUrl, {
+    method: "GET",
+  });
+  if (res.ok) {
+    const fetchedData = await res.json();
+    console.log(fetchedData);
+    content(fetchedData);
+  } else {
+    console.log("error");
+  }
+}
+
+fetchData();
