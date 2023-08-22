8192d3d7cd20cbb609d3d91aaded508835edd9f4
festerduck <festerduck@gmail.com>
2023-08-22 20:47:52 +0500

    Search Box is added

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
+const body = document.querySelector("body");
+const container = document.querySelector(".container");
+const searchIcon = document.getElementById("search-icon");
+const searchBox = document.getElementById("search-box");
+const search = document.querySelector(".hide-search");
+
+function searchMechanism(show) {
+  if (show) {
+    body.classList.add("show-elements");
+    search.classList.add("show-search");
+    search.classList.remove("hide-search");
+    search.addEventListener("click", (e) => {
+      e.stopPropagation();
+    });
+    searchBox.focus();
+  } else {
+    body.classList.remove("show-elements");
+    search.classList.remove("show-search");
+    search.classList.add("hide-search");
+    searchBox.value = "";
+  }
+}
+
+searchIcon.addEventListener("click", (e) => {
+  e.stopPropagation();
+  searchMechanism(true);
+  body.addEventListener("click", (e) => {
+    e.stopImmediatePropagation();
+    searchMechanism(false);
+  });
+});
+
 function content(data) {
   const currentLocation = document.getElementById("current-location");
   const currentTemp = document.getElementById("current-temp");
